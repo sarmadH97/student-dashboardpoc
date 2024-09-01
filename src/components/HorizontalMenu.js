a88c7c4bb589515d00357ca3abf4b3d2a2f8b80c
Sarmad Hussain <sarmadhusssain@gmail.com>
2024-09-01 22:47:41 +0500

    Routing issue

--- src/components/HorizontalMenu.js
+++ src/components/HorizontalMenu.js
@@ -4,10 +4,10 @@ function HorizontalMenu() {
   return (
     <div className="horizontal-menu p-4 text-white bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer duration-300">
       <div className="container mx-auto flex justify-end space-x-8">
-        <a href="/" className="hover:text-gray-400">Home</a>
-        <a href="/" className="hover:text-gray-400">Profile</a>
-        <a href="/" className="hover:text-gray-400">Settings</a>
-        <a href="/" className="hover:text-gray-400">Logout</a>
+        <a className="hover:text-gray-400">Home</a>
+        <a className="hover:text-gray-400">Profile</a>
+        <a className="hover:text-gray-400">Settings</a>
+        <a className="hover:text-gray-400">Logout</a>
       </div>
     </div>
   );
